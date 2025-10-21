7b224e076e19dbcc59ff64f97b074d31abd552c8
manuhansum <hansummanuel@gmail.com>
2025-10-21 18:49:12 +0200

    initialized spring boot

---
+++ einkauf-service/src/main/java/fhdw/de/einkauf_service/EinkaufServiceApplication.java
@@ -0,0 +1,13 @@
+package fhdw.de.einkauf_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinkaufServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EinkaufServiceApplication.class, args);
+	}
+
+}
---
+++ einkauf-service/src/test/java/fhdw/de/einkauf_service/EinkaufServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fhdw.de.einkauf_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinkaufServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
