e4e58fc01303c4b3885d394bb428061b82127305
Rohid <rohid.bachtjar@edu.fhdw.de>
2025-10-21 18:46:46 +0200

    Start vom Kassenkrach

---
+++ src/main/java/de/fhdw/kassensystem/view/MainView.java
@@ -0,0 +1,9 @@
+package de.fhdw.kassensystem.view;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+    public MainView() {}
+}
