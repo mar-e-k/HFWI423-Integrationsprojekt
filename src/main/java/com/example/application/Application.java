adbdc3990090e31155f6bd4af10269c676397cf9
Jasototeles <jason.luck@edu.fhdw.de>
2025-10-28 21:00:46 +0100

    Test rückgängig

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Theme(value = "my-app")
 public class Application implements AppShellConfigurator {
-//lol
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
