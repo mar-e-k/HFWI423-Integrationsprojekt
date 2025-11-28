8cd0a0fd6f4cea1ea86486ebab1007925309c154
rohidb10 <rohid.bachtjar@edu.fhdw.de>
2025-11-28 09:19:18 +0100

    feat(store initializer): only initializing one default store

--- filialensystem/src/main/java/de/fhdw/fillialensystem/utility/initializer/StoreInitializer.java
+++ filialensystem/src/main/java/de/fhdw/fillialensystem/utility/initializer/StoreInitializer.java
@@ -23,11 +23,13 @@ public class StoreInitializer implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        log.info("Deleting all existing stores.");
-        storeService.deleteAll();
-        log.info("All stores deleted. Creating default store.");
-        Store defaultStore = new Store("default-store", "Hauptfiliale");
-        storeService.save(defaultStore);
-        log.info("Default store created successfully.");
+        if (storeService.count() == 0) {
+            log.info("No stores found. Creating default store.");
+            Store defaultStore = new Store("default-store", "Hauptfiliale");
+            storeService.save(defaultStore);
+            log.info("Default store created successfully.");
+        } else {
+            log.info("Stores already exist. Skipping default store creation.");
+        }
     }
 }
