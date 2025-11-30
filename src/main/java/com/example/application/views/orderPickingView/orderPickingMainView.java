c611b11e3c29ed63e5ffc16b970b3fa7ea6aa8c3
admin <lars.jaouki@edu.fhdw.de>
2025-11-30 22:13:06 +0100

    kommission neue DB

--- src/main/java/com/example/application/views/orderPickingView/orderPickingMainView.java
+++ src/main/java/com/example/application/views/orderPickingView/orderPickingMainView.java
@@ -19,6 +19,7 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.vaadin.lineawesome.LineAwesomeIconUrl;
 
+
 import java.util.Comparator;
 import java.util.List;
 
