73c38bef368b313fc7f69d94845281ce55e93f8a
rohidb10 <rohid.bachtjar@edu.fhdw.de>
2026-03-25 13:02:00 +0100

    RootView: updated RootView to test messaging with Logistik

--- store/ui/src/main/java/de/fhdw/vendix/store/ui/RootView.java
+++ store/ui/src/main/java/de/fhdw/vendix/store/ui/RootView.java
@@ -6,6 +6,8 @@ import de.fhdw.vendix.commons.ui.vaadin.view.AbstractRootView;
 import io.github.plaguv.amqp.api.envelope.EventEnvelope;
 import io.github.plaguv.amqp.api.envelope.EventEnvelopeBuilder;
 import io.github.plaguv.amqp.api.event.logistic.ArticleSentEvent;
+import io.github.plaguv.amqp.api.event.pos.ArticleOrderEvent;
+import io.github.plaguv.amqp.api.event.pos.ArticleUrgentOrderEvent;
 import io.github.plaguv.amqp.core.publisher.EventPublisher;
 import jakarta.annotation.security.RolesAllowed;
 
@@ -17,19 +19,37 @@ public class RootView extends AbstractRootView {
 
     public RootView(EventPublisher publisher) {
         this.publisher = publisher;
-        configureSubmitButton();
+        configureSubmitOrderButton();
+        configureSubmitUrgentOrderButton();
     }
 
-    private void configureSubmitButton() {
+    private void configureSubmitOrderButton() {
         Button submitButton = new Button("Submit");
         submitButton.addClickListener(event -> {
-                    ArticleSentEvent articleSentEvent = new ArticleSentEvent(
+                    ArticleOrderEvent articleOrderEvent = new ArticleOrderEvent(
                             1L,
                             1L,
                             5
                     );
                     EventEnvelope envelope = EventEnvelopeBuilder.defaults()
-                            .withContent(articleSentEvent)
+                            .withContent(articleOrderEvent)
+                            .build();
+                    publisher.publishMessage(envelope);
+                }
+        );
+        add(submitButton);
+    }
+
+    private void configureSubmitUrgentOrderButton() {
+        Button submitButton = new Button("Submit");
+        submitButton.addClickListener(event -> {
+                    ArticleUrgentOrderEvent articleUrgentOrderEvent = new ArticleUrgentOrderEvent(
+                            1L,
+                            1L,
+                            5
+                    );
+                    EventEnvelope envelope = EventEnvelopeBuilder.defaults()
+                            .withContent(articleUrgentOrderEvent)
                             .build();
                     publisher.publishMessage(envelope);
                 }
