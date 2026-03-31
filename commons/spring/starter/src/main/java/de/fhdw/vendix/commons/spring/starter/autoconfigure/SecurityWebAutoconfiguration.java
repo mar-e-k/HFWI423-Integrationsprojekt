002086beba85b722cd79aba9d24ea4cc7df1a5b5
erik.gruber <erik.ruber1337@gmail.com>
2026-03-31 17:54:19 +0200

    feat(store): added configs for promtail and prometheus
    feat(commons-security): added filterchain for actuator impl

--- commons/spring/starter/src/main/java/de/fhdw/vendix/commons/spring/starter/autoconfigure/SecurityWebAutoconfiguration.java
+++ commons/spring/starter/src/main/java/de/fhdw/vendix/commons/spring/starter/autoconfigure/SecurityWebAutoconfiguration.java
@@ -6,26 +6,44 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @AutoConfiguration(after = SecurityAutoConfiguration.class)
 public class SecurityWebAutoconfiguration {
 
+
     @Bean
     @Order(0)
+    public SecurityFilterChain actuatorSecurity(HttpSecurity http) {
+        return http
+                .securityMatcher("/actuator/**")
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .build();
+    }
+
+    @Bean
+    @Order(1)
     public SecurityFilterChain apiSecurity(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) {
         return http
                 .securityMatcher("/api/**")
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().authenticated()
                 )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
     @Bean
-    @Order(1)
+    @Order(2)
     public SecurityFilterChain vaadinSecurity(HttpSecurity http) {
         return http
                 .with(VaadinSecurityConfigurer.vaadin(), configurer -> configurer
--- pos/app/src/main/java/de/fhdw/vendix/pos/PosApplication.java
+++ pos/app/src/main/java/de/fhdw/vendix/pos/PosApplication.java
@@ -2,8 +2,10 @@ package de.fhdw.vendix.pos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class PosApplication {
     public static void main(String[] args) {
         SpringApplication.run(PosApplication.class, args);
